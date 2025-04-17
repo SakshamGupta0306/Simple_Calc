451add81f5ae0c1a1ccb4d2da31469e24bc727f1
Saksham@Git <gupta0306saksham@gmail.com>
2025-04-18 00:54:07 +0530

    This Follows the Dmas System with more organised way

--- Simple_calc.c
+++ Simple_calc.c
@@ -1,68 +1,118 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int addn(float num1,float num2){
+float addn(float num1,float num2){
     return num1+num2;
 }
 
-int subs(float num1,float num2){
+float subs(float num1,float num2){
     return num1-num2;
 }
 
-int mult(float num1,float num2){
+float mult(float num1,float num2){
     return num1*num2;
 }
 
-float div(float num1,float num2){
+float divide(float num1,float num2){
     if (num2==0){
         printf("Error, Division with 0 is not possible.");
+        exit(1);
     }
     else{
         return num1/num2;
     }
-    return 0;
 }
 
+
+void shift_elements(float num[],char ops[],int start,int end){
+    for (int k = start; k < end; k++) {
+        num[k] = num[k + 1];
+        ops[k] = ops[k + 1];
+    }
+}
 
 
 int main(){
     float num[200];
-    float res;
-    char ops[200]; 
+    float res=0;
+    char ops[200];
     int i=0;
-    printf("What calculations you want to do & end with = sign (eg 5+3-2/4=):");
+    printf("Write your Equation in form of (2+3-4=) No negative numbers:");
     scanf("%f %c",&num[i],&ops[i]);
     i++;
+
+    //Reading the Expression
     while (1){
-        scanf("%f %c",&num[i],&ops[i]);/*This takes the multiple input from the user and stores in the arrays*/
+        scanf("%f %c",&num[i],&ops[i]);
         if (ops[i]=='='){
             break;
         }
         i++;
     }
+
+    int Total_elements=i;
+    
+    //Printing the same expression
+    printf("Expression:");
+    for (int j=0; j<=i ; j++){
+        printf("%.2f %c ",num[j],ops[j]);
+    }
+    printf("\n");
+
+    //Operation Precedence
+
+    //Division
+    for (int j=0;j<i;j++){
+        if (ops[j] == '/'){
+            res=divide(num[j],num[j+1]);
+            ops[j]=ops[j+1];
+            num[j]=res;
+            shift_elements(num,ops,j+1,i);
+            i--,j--;
+            }
+        }
+
+    //Multiplication
+    for (int j=0;j<i;j++){
+        if (ops[j] == '*'){
+            res=mult(num[j],num[j+1]);
+            ops[j]=ops[j+1];
+            num[j]=res;
+            shift_elements(num,ops,j+1,i);
+            i--,j--;
+        }
+    }
+
+    //Subtraction
+    for (int j=0;j<i;j++){
+        if (ops[j] == '-'){
+            res=subs(num[j],num[j+1]);
+            ops[j]=ops[j+1];
+            num[j]=res;
+            shift_elements(num,ops,j+1,i);
+            i--,j--;
+        }
+    }
+
+    //Addition
     for (int j=0;j<i;j++){
         if (ops[j] == '+'){
             res=addn(num[j],num[j+1]);
+            ops[j]=ops[j+1];
+            num[j]=res;
+            shift_elements(num,ops,j+1,i);
+            i--,j--;
         }
-        else if (ops[j] == '-'){
-            res=subs(num[j],num[j+1]);
-        }
-        else if (ops[j] == '*'){
-            res=mult(num[j],num[j+1]);
-        }
-        else if (ops[j] == '/'){
-            res=div(num[j],num[j+1]);
-            if (num[j+1]==0){
-                return 0;
-            }
-        }
-        else{
-            printf ("You used an invalid operator.");
-            return 0;
-        }
-        num[j+1]=res;
     }
 
-    printf(" %.2f \n",num[i]);
-    printf ("END");
+
+    //for (int j=0;j<= Total_elements;j++){
+    //    printf(" %f %d \n",num[j],j);
+    //}
+
+    //Result
+    printf("Result: %.2f \n",num[0]);
+    printf ("END\n");
     return 0;
 }
+
